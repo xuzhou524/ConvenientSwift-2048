539e3f57b1f25271ec245ee8c9141c0663dbf713
xuzhou524 <xuuohz@163.com>
2016-05-17 16:56:57 +0800

    first commit

---
+++ ConvenientSwift-2048/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ConvenientSwift-2048
+//
+//  Created by gozap on 16/5/17.
+//  Copyright © 2016年 xuzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.startGameButtonTapped()
+    }
+    func startGameButtonTapped() {
+        let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
+        self.presentViewController(game, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# ConvenientSwift-2048
