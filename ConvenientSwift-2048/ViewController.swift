e17b1983de8c7d75ae9b977f1421fb4fa179fdad
xuzhou524 <xuuohz@163.com>
2016-05-24 14:37:42 +0800

    bak

--- ConvenientSwift-2048/ViewController.swift
+++ ConvenientSwift-2048/ViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.startGameButtonTapped()
     }
+    
     func startGameButtonTapped() {
         let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
         self.presentViewController(game, animated: true, completion: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
